94fa8e9fdd96fa20940ca5f72af174be259b0da1
Redtronics <development@redtronics.de>
2023-03-12 22:20:57 +0100

    update: add class Ticket

--- src/loader/jsonLoader.py
+++ src/loader/jsonLoader.py
@@ -8,3 +8,12 @@ class Token:
 
     def get_token(self):
         return self.__config["token"]
+
+
+class Ticket:
+    def __init__(self):
+        with open("resources/text/ticket.json", "r") as c:
+            self.__config = json.load(c)
+
+    def get_ticket(self):
+        return self.__config
