be062f6df48582f8359c5f48c86e415a5ff9ca69
Redtronics <development@redtronics.de>
2023-03-28 18:28:28 +0200

    Initial commit

--- src/loader/jsonLoader.py
+++ src/loader/jsonLoader.py
@@ -11,12 +11,3 @@ class Token:
 
     def maria_db(self):
         return self.__config["mariadb"]["host"], self.__config["mariadb"]["user"], self.__config["mariadb"]["password"], self.__config["mariadb"]["database"]
-
-
-class TicketText:
-    def __init__(self):
-        with open("resources/text/ticket.json", "r") as c:
-            self.__config = json.load(c)
-
-    def get_ticket(self):
-        return self.__config
