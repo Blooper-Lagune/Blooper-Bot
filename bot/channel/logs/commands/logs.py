e69af3855cb21a12ab675d3bc7f181cc5ff1d951
Redtronics <development@redtronics.de>
2023-04-01 18:35:15 +0200

    fix: reformat code

--- bot/channel/logs/commands/logs.py
+++ bot/channel/logs/commands/logs.py
@@ -12,19 +12,32 @@ class Logs(commands.Cog):
             pool_size=9
         )
 
-    async def get_channel(self, message: nextcord.Message) -> None | nextcord.TextChannel:
+    async def get_channel(
+            self,
+            message: nextcord.Message
+    ) -> None | nextcord.TextChannel:
+
         channel = self.database.execute(
             query="SELECT channel FROM log_channel",
             data=[]
         )
+
         if not channel or message.author.bot:
             return
 
-        return nextcord.utils.get(message.guild.channels, id=channel[0][0])
+        return nextcord.utils.get(
+            message.guild.channels,
+            id=channel[0][0]
+        )
 
     @commands.Cog.listener()
-    async def on_message(self, message: nextcord.Message):
+    async def on_message(
+            self,
+            message: nextcord.Message
+    ) -> None:
+
         channel: nextcord.TextChannel = await self.get_channel(message=message)
+
         if not channel:
             return
 
@@ -34,12 +47,22 @@ class Logs(commands.Cog):
             color=nextcord.Color.orange(),
             description=""
         )
+
         embed_logs.title = "Neuer Log"
-        embed_logs.add_field(name=f"Neue Nachricht in {message.channel.mention}", value=message.content)
+        embed_logs.add_field(
+            name=f"Neue Nachricht in {message.channel.mention}",
+            value=message.content
+        )
+
         await channel.send(embed=embed_logs)
 
     @commands.Cog.listener()
-    async def on_message_edit(self, before: nextcord.Message, after: nextcord.Message):
+    async def on_message_edit(
+            self,
+            before: nextcord.Message,
+            after: nextcord.Message
+    ) -> None:
+
         channel: nextcord.TextChannel = await self.get_channel(message=after)
 
         if not channel:
@@ -51,13 +74,24 @@ class Logs(commands.Cog):
             color=nextcord.Color.orange(),
             description=""
         )
+
         embed_logs.title = "Neuer Log"
-        embed_logs.add_field(name=f"Nachricht bearbeitet in {before.channel.mention} von:", value=before.content, inline=False)
-        embed_logs.add_field(name="Zu:", value=after.content)
+        embed_logs.add_field(
+            name=f"Nachricht bearbeitet in {before.channel.mention} von:",
+            value=before.content, inline=False)
+        embed_logs.add_field(
+            name="Zu:",
+            value=after.content
+        )
+
         await channel.send(embed=embed_logs)
 
     @commands.Cog.listener()
-    async def on_message_delete(self, message: nextcord.Message):
+    async def on_message_delete(
+            self,
+            message: nextcord.Message
+    ) -> None:
+
         channel: nextcord.TextChannel = await self.get_channel(message=message)
 
         if not channel:
@@ -69,8 +103,14 @@ class Logs(commands.Cog):
             color=nextcord.Color.orange(),
             description=""
         )
+
         embed_logs.title = "Neuer Log"
-        embed_logs.add_field(name=f"Nachricht gelöscht in {message.channel.mention}:", value=message.content, inline=False)
+        embed_logs.add_field(
+            name=f"Nachricht gelöscht in {message.channel.mention}:",
+            value=message.content,
+            inline=False
+        )
+
         await channel.send(embed=embed_logs)
 
 
